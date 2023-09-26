16720ae235a44da09ca1f34eba39ca84d063f47f
karen <klisse22@jaumebalmes.net>
2023-09-26 23:56:55 +0200

    segundo

--- practica2.php
+++ practica2.php
@@ -8,29 +8,42 @@
     <h2>Numero de cursos apuntats: 3</h2>
 </head>
 <body>
-    <ul>
     <?php 
     define("IVA", 0.21);
-    $lenguajes=array(
-    array("PHP", "Introducció a PHP", "15.5"),
-    array("Laravel", "Laravel per experts", "30"),
-    array("Django", "Dominant Django", "10")
+    $cursos = array(
+        array("ID" => 1, "NOM" => "PHP", "DESC" => "Introducció a PHP", "PREU" => 15.5),
+        array("ID" => 2, "NOM" => "Laravel", "DESC" => "Laravel per experts", "PREU" => 30),
+        array("ID" => 3, "NOM" => "Django", "DESC" => "Dominant Django", "PREU" => 10)
     );
     ?>
     <div>
-    <li>
-        <?php
-        for(i=0; i<3; $i++){
-        $lenguajes[$i];
-  }
-        ?> 
-    </div>
-    </li>
-    <li>
-    <?php
+    <?php 
+     function calcularTotalCursos($cursos) {
+        return count($cursos);
+    }
 
+    $imagenes = array(
+        "id1.jpg",
+        "id2.jpg",
+        "id3.jpg"
+    );
+
+      for ($i = 0; $i<3; $i++) {
+        $curs = $cursos[$i];
+        $nomCurs = $curs["NOM"];
+        $descripcio = $curs["DESC"];
+        $preuSenseIVA = $curs["PREU"];
+  
+        $preuAmbIVA = $preuSenseIVA + ($preuSenseIVA * IVA);
+        
+        echo "<div style='border: 1px solid black;'>";
+        echo "<img src='" . $imagenes[$i] . "' alt='$nomCurs' />"; 
+        echo "<h3>$nomCurs</h3>";
+        echo "<p>$descripcio</p>";
+        echo "<p>Precio con IVA: $preuAmbIVA €</p>";
+        echo "</div>";
+    }
     ?>
-    </li>
-    </ul> 
+    </div> 
 </body>
 </html>
